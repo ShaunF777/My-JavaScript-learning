16b7a6b4bf44ef63a90c6c738b8c3cccb7591856
ShaunF777 <shaunfr777@gmail.com>
2024-10-30 19:20:07 +0200

    add to functions for monsters in games script.js

--- 02game/script.js
+++ 02game/script.js
@@ -25,6 +25,23 @@ const weapons = [
     { name: 'claw hammer', power: 50 },
     { name: 'sword', power: 100 }
   ];
+const monsters = [
+    {
+        name: "slime",
+        level: 2,
+        health: 15
+    },
+    {
+        name: "fanged beast",
+        level: 8,
+        health: 60
+    },
+    {
+        name: "dragon",
+        level: 20,
+        health: 300
+    }
+];
 const locations = [
   {
     name: "town square",
@@ -37,6 +54,18 @@ const locations = [
     "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
     "button functions": [buyHealth, buyWeapon, goTown],
     text: "You enter the store."
+  }
+  {
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. You see some monsters."
+  },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster."
   }
 ];
 
@@ -65,10 +94,6 @@ function goTown() {
   
   function goCave() {
     update(locations[2]);
-  }
-  
-  function fightDragon() {
-    console.log("Fighting dragon.");
   }
   
   function buyHealth() {
@@ -102,12 +127,37 @@ function goTown() {
       button2.onclick = sellWeapon;
     }
   }
+  function sellWeapon() {
+    if (inventory.length > 1) {
+      gold += 15;
+      goldText.innerText = gold;
+      let currentWeapon = inventory.shift();
+      text.innerText = "You sold a " + currentWeapon + ".";
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "Don't sell your only weapon!";
+    }
+  }
   
-
   function fightSlime() {
-
+    fighting = 0;
+    goFight();
   }
   
   function fightBeast() {
+    fighting = 1;
+    goFight();
+  }
+  
+  function fightDragon() {
+    fighting = 2;
+    goFight();
+  }
+
+  function attack() {
+
+  }
+  
+  function dodge() {
   
   }
