1f837e23ffd5fd30247d461f9e66a1d659e7518c
ShaunF777 <shaunfr777@gmail.com>
2024-10-30 15:48:42 +0200

    add to function buyWeapon in games script.js

--- 02game/script.js
+++ 02game/script.js
@@ -16,6 +16,15 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+/**84The value of the currentWeaponIndex variable corresponds to an index in the weapons 
+ * array. The player starts with a "stick", since currentWeaponIndex starts at 0 and 
+ * weapons[0] is the "stick" weapon. */
+const weapons = [
+    { name: 'stick', power: 5 },
+    { name: 'dagger', power: 30 },
+    { name: 'claw hammer', power: 50 },
+    { name: 'sword', power: 100 }
+  ];
 const locations = [
   {
     name: "town square",
@@ -73,9 +82,27 @@ function goTown() {
     }
   }
   
+  /**Once a player has the best weapon, they cannot buy another one.  */
   function buyWeapon() {
-  
+    if (currentWeaponIndex < weapons.length - 1) {
+      if (gold >= 30) {
+        gold -= 30;
+        currentWeaponIndex++;
+        goldText.innerText = gold;
+        let newWeapon = weapons[currentWeaponIndex].name;
+        text.innerText = "You now have a " + newWeapon + ".";
+        inventory.push(newWeapon);
+        text.innerText += " In your inventory you have: " + inventory;
+      } else {
+        text.innerText = "You do not have enough gold to buy a weapon.";
+      }
+    } else {
+      text.innerText = "You already have the most powerful weapon!";
+      button2.innerText = "Sell weapon for 15 gold";
+      button2.onclick = sellWeapon;
+    }
   }
+  
 
   function fightSlime() {
 
